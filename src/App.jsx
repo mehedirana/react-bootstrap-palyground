22843ae8e8b207a0a96b00dd4ce818c958396c16
mehedirana <mdmehedirana@gmail.com>
2025-01-27 13:07:48 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // This includes Popper.js
+import NavBar from './components/navbar/NavBar';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div>
+      <NavBar />
+
+
+      <div class="card" aria-hidden="true">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title placeholder-glow">
+            <span class="placeholder col-6"></span>
+          </h5>
+          <p class="card-text placeholder-glow">
+            <span class="placeholder col-7"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-6"></span>
+            <span class="placeholder col-8"></span>
+          </p>
+          <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -40,7 +44,7 @@ function App() {
       <button type="button" class="btn btn-dark">Dark</button>
 
       <button type="button" class="btn btn-link">Link</button>
-    </>
+    </div>
   )
 }
 
